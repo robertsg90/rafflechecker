13670ed9b19145e70c9bfb4579bb2b125bc8f69c
=robertsg90 <robertsg90@users.noreply.github.com>
2020-10-31 23:02:20 -0500

    Adding initial source

---
+++ src/RaffleClass.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class RaffleClass {
+    int ownedRaffleNums;  // Create a class attribute
+
+    // Create a class constructor for the MyClass class
+    public RaffleClass(int raffleNum) {
+        ownedRaffleNums = raffleNum;  // Set the initial value for the class attribute x
+    }
+
+}
